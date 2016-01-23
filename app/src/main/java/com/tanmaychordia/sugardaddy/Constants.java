e0b54133dc19aebad691d6d9d10837fdf9ca35dc
aleckwang <wangalec19@gmail.com>
2016-01-23 05:00:48 -0500

    Add Houndify Extras

---
+++ app/src/main/java/com/tanmaychordia/sugardaddy/Constants.java
@@ -0,0 +1,9 @@
+package com.tanmaychordia.sugardaddy;
+
+/**
+ * General constants.
+ */
+public class Constants {
+    public static final String CLIENT_ID = "MPX6IP3bSJH6raRTDG8Qnw==";
+    public static final String CLIENT_KEY = "q06qB8u1QBZdagGu8ayNkBHVx7uAAT8D8VW7xutgjZ-EKkc7ZSRhsI8jpWzKhv9usI_BbxrdS0ymLvk584PWvw==";
+}
